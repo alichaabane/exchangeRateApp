d6bef50a9d1253162cf218b6b8694a862ccd0f69
alichaabane98 <alichaabane98@gmail.com>
2025-04-18 10:21:00 +0300

    add pwa p1

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/ziraat-api': {
-        target: 'https://www.ziraatbank.com.tr',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace(/^\/ziraat-api/, '')
-      }
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.png'],
+            manifest: {
+                name: 'My Doviz App',
+                short_name: 'DovizApp',
+                description: 'Live currency rates from Ziraat and Global APIs',
+                theme_color: '#c10000',
+                icons: [
+                    {
+                        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIb8DmDSvvBVjnylmjpk-QG1QlRmVgAaxFNw&s',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIb8DmDSvvBVjnylmjpk-QG1QlRmVgAaxFNw&s',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })],
+    server: {
+        proxy: {
+            '/ziraat-api': {
+                target: 'https://www.ziraatbank.com.tr',
+                changeOrigin: true,
+                secure: false,
+                rewrite: path => path.replace(/^\/ziraat-api/, '')
+            }
+        }
     }
-  }
 })
